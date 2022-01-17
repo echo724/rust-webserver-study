233b069a887f358284e0ee64c3c95a42c50e3171
echo724 <eunchan1001@gmail.com>
2022-01-17 19:36:25 +0900

    feat: Create response as html

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener,TcpStream};
 use std::io::prelude::*;
+use std::fs;
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,8 +15,10 @@ fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 512]; //[0,0,0,...*512]
 
     stream.read(&mut buffer).unwrap();
-    
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();

e94b8ce1395723cd410aae0f7bdc03674b009512
echo724 <eunchan1001@gmail.com>
2022-01-17 19:23:22 +0900

    feat: Create handle_connection to read request

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use std::net::TcpListener;
+use std::net::{TcpListener,TcpStream};
+use std::io::prelude::*;
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Conntected!")
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer = [0; 512]; //[0,0,0,...*512]
+
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}",String::from_utf8_lossy(&buffer[..]))
 }
